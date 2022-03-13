ba8d6650972a7e576e28cbfd7ba6e47a38098624
tammysu249 <tammy.su@kellogg.northwestern.edu>
2022-03-13 15:13:13 +0000

    bootstrapped

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    if session[:user_id]
-    @current_user = User.find(session[:user_id]) 
+      @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
