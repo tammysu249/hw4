8e7f50efda3ffc2f08461fde326fa05c831a56f3
tammysu249 <tammy.su@kellogg.northwestern.edu>
2022-03-13 03:13:43 +0000

    logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
-        flash[:notice] = "Welcome, #{@user.first_name}."
-        redirect_to "/companies"
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
       else
         flash[:notice] = "Wrong password"
         redirect_to "/sessions/new"
